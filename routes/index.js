c70855a174e80082b616eb44339356a8190b05eb
sonugopal <sonugopal41@gmail.com>
2019-07-19 23:33:09 +0530

    user-registration

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/user-registration', function(req, res, next) {
+  console.log("API called");
+    if(req.body){
+      console.log(req.body.fname);
+        res.json({"status":true});
+    }
+    else{
+      console.log("no data found");
+        res.json({"status":false})
+    }
+
+});
+
 module.exports = router;
