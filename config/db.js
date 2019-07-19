c622e5fedc43f5f89b277f1b41eb09fbe437bd4a
sonugopal <sonugopal41@gmail.com>
2019-07-20 02:47:28 +0530

    colleges page added

--- config/db.js
+++ config/db.js
@@ -18,21 +18,21 @@ db.query('create database if not exists Trip_Pool', function(err, result) {
 db.query('use Trip_Pool',function (err, result) {
     if (err) throw err
 });
-var query='create table if not exists Cities(city_id int primary key,city_name varchar(255))';
+var query='create table if not exists Cities(city_id int primary key NOT NULL AUTO_INCREMENT,city_name varchar(255))';
 db.query(query,function (err, result) {
     if (err) throw err
     //console.log(result)
 
 })
 
-query='create table if not exists Colleges(college_id int primary key,college_name varchar(255),college_address varchar(255),town varchar(255))'
+query='create table if not exists Colleges(college_id int primary key NOT NULL AUTO_INCREMENT,college_name varchar(255),college_address varchar(255),town varchar(255))'
 db.query(query,function (err, result) {
     if (err) throw err
     //console.log(result)
 
 })
 
-query='create table if not exists Users(user_id int primary key,' +
+query='create table if not exists Users(user_id int primary key NOT NULL AUTO_INCREMENT,' +
     'first_name varchar(255),' +
     'last_name varchar(255),' +
     'email varchar(255),' +
--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,10 @@ const db=require('../config/db');
 const conn=db.db;
 /* GET users listing. */
 router.get('/cities', function(req, res, next) {
-    console.log(conn);
+
     conn.query('SELECT * FROM Cities', function(err, results) {
         if (err) throw err
+        console.log(results)
         res.render('cities',{'Cities':results});
 
     })
@@ -20,5 +21,52 @@ router.get('/cities', function(req, res, next) {
 
 
 });
+router.get('/add-city',function (req,res,next) {
+    res.render("add-city");
+})
+router.post('/doAddCity',function (req,res,next) {
+    if(req.body){
+        query="INSERT INTO `Trip_Pool`.`Cities` (`city_name`) VALUES ('"+req.body.CityName+"')";
+        conn.query(query,function (err, result) {
+            if (err) throw err
+            //console.log(result)
+            console.log(result)
+            res.redirect("/admin/cities");
 
+        })
+    }
+})
+router.get('/colleges',function (req,res,next) {
+    query="SELECT Colleges.college_id,Colleges.college_name,Colleges.college_address,Cities.city_name FROM Colleges,Cities WHERE Colleges.town=Cities.city_id;";
+    conn.query(query, function(err, results) {
+        if (err) throw err
+        console.log(results)
+        res.render('colleges',{'Colleges':results});
+
+    })
+})
+router.get('/add-college',function (req,res,next) {
+    conn.query('SELECT * FROM Cities', function(err, results) {
+        if (err) throw err
+        console.log(results)
+        res.render('add-college',{'Cities':results});
+
+    })
+
+})
+router.post('/doAddCollege',function (req,res,next) {
+    if(req.body){
+        var name=req.body.Name;
+        var adds=req.body.Address;
+        var city=req.body.City;
+        query="INSERT INTO `Trip_Pool`.`Colleges` (`college_name`, `college_address`, `town`) VALUES ('"+name+"', '"+adds+"', '"+city+"');"
+        conn.query(query,function (err, result) {
+            if (err) throw err
+            //console.log(result)
+            console.log(result)
+            res.redirect("/admin/colleges");
+
+        })
+    }
+})
 module.exports = router;
