74a89049b4dc4959a65b127346a617366cc6b81a
sonugopal <sonugopal41@gmail.com>
2019-09-24 22:31:35 +0530

    find-requests

--- routes/index.js
+++ routes/index.js
@@ -264,10 +264,17 @@ router.get("/view-requests",function (req,res) {
     var query="select u.first_name,u.mobile,u.college from Users u,Trips t,Requests r where t.user='"+driver+"' && r.trip_d=t.trip_id && u.user_id=r.user_id"
     conn.query(query,function (err,data) {
         if(!err){
+            console.log(data);
             var obj={};
             obj.data=data;
             obj.status=true;
+            console.log(obj);
             res.json(obj);
+        }
+        else{
+            console.log(err);
+            res.json(obj);
+
         }
     })
 })
